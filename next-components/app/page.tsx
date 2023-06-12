d655d43143115a244f364844c5ec4e41f78e3ee2
zacharywalsh13 <zacharywalsh13@gmail.com>
2023-06-12 11:53:02 -0500

    First Commit

---
+++ next-components/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MyComponent from '../components/MyComponent'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>My Next.js App</h1>
+      <MyComponent />
+    </div>
+  )
+}
---
+++ next-components/components/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyComponent = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded">
+      Hello from MyComponent
+    </div>
+  )
+}
+
+export default MyComponent
